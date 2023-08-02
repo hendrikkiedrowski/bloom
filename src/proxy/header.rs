b3d689e97499e4c5e9d7671e451e3cc631723568
hendrikvonkiedrowski <hendrikkiedrowski@gmail.com>
2023-08-02 20:33:16 +0200

    added lowercase comparison for auth header

--- src/proxy/header.rs
+++ src/proxy/header.rs
@@ -4,11 +4,10 @@
 // Copyright: 2017, Valerian Saliou <valerian@valeriansaliou.name>
 // License: Mozilla Public License v2.0 (MPL v2.0)
 
-use std::collections::HashSet;
 use hyper::header::{ETag, Header, Vary};
 use hyper::Headers;
 use std::str::from_utf8;
-use unicase::{Ascii, UniCase};
+use unicase::{Ascii};
 
 use super::defaults;
 use crate::{header::request_shard::HeaderRequestBloomRequestShard, APP_CONF};
@@ -19,16 +18,21 @@ impl ProxyHeader {
     pub fn parse_from_request(headers: Headers) -> (Headers, String, u8) {
         // Request header: 'Authorization'
         //debug!("Request headers {}", headers);
-        for x in headers.iter() {
-            debug!("{}", x.name())
+        fn get_auth_header(headers: &Headers) -> String {
+            let mut auth_header = defaults::REQUEST_AUTHORIZATION_DEFAULT.to_string();
+            for x in headers.iter() {
+                if x.name().to_lowercase() == &*APP_CONF.cache.auth_header.to_lowercase() {
+                    debug!("Authorization header found");
+                    auth_header = from_utf8(x.raw().one().unwrap_or(&[])).unwrap_or(defaults::REQUEST_AUTHORIZATION_DEFAULT).to_string();
+                } else {
+                    debug!("Authorization header not found");
+                    auth_header = defaults::REQUEST_AUTHORIZATION_DEFAULT.to_string();
+                }
+            }
+            return auth_header;
         }
 
-        let auth = match headers.get_raw(&*APP_CONF.cache.auth_header) {
-            None => defaults::REQUEST_AUTHORIZATION_DEFAULT,
-            Some(value) => from_utf8(value.one().unwrap_or(&[]))
-                .unwrap_or(defaults::REQUEST_AUTHORIZATION_DEFAULT),
-        }
-        .to_string();
+        let auth = get_auth_header(&headers);
 
         // Request header: 'Bloom-Request-Shard'
         let shard = match headers.get::<HeaderRequestBloomRequestShard>() {
